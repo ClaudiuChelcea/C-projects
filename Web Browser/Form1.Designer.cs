db790d4968a2a428b973f470527e4f6e891dde3c
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-07-05 12:21:04 +0300

    Add browser

---
+++ Web Browser/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.BingBrowser = new System.Windows.Forms.WebBrowser();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.ForwardButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(48, 132);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(914, 590);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.BingBrowser);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(906, 562);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Bing";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // BingBrowser
+            // 
+            this.BingBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BingBrowser.Location = new System.Drawing.Point(3, 3);
+            this.BingBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.BingBrowser.Name = "BingBrowser";
+            this.BingBrowser.Size = new System.Drawing.Size(900, 556);
+            this.BingBrowser.TabIndex = 0;
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, System.Drawing.FontStyle.Bold);
+            this.SearchTextBox.Location = new System.Drawing.Point(167, 58);
+            this.SearchTextBox.Multiline = true;
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(608, 56);
+            this.SearchTextBox.TabIndex = 0;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.BackButton.Location = new System.Drawing.Point(48, 58);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(50, 56);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "<<";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // ForwardButton
+            // 
+            this.ForwardButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.ForwardButton.Location = new System.Drawing.Point(104, 58);
+            this.ForwardButton.Name = "ForwardButton";
+            this.ForwardButton.Size = new System.Drawing.Size(50, 56);
+            this.ForwardButton.TabIndex = 3;
+            this.ForwardButton.Text = ">>";
+            this.ForwardButton.UseVisualStyleBackColor = true;
+            this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.SearchButton.Location = new System.Drawing.Point(781, 58);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(73, 56);
+            this.SearchButton.TabIndex = 4;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.RefreshButton.Location = new System.Drawing.Point(860, 58);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(79, 56);
+            this.RefreshButton.TabIndex = 5;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.HomeButton.Location = new System.Drawing.Point(388, 12);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(184, 40);
+            this.HomeButton.TabIndex = 1;
+            this.HomeButton.Text = "HOME";
+            this.HomeButton.UseVisualStyleBackColor = true;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Image = global::WindowsFormsApp1.Properties.Resources.Close_Button;
+            this.CloseButton.Location = new System.Drawing.Point(926, 12);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(46, 35);
+            this.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(984, 734);
+            this.ControlBox = false;
+            this.Controls.Add(this.HomeButton);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ForwardButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.CloseButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MinimumSize = new System.Drawing.Size(1000, 750);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rose Quartz Browser";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CloseButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button ForwardButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.WebBrowser BingBrowser;
+    }
+}
+
---
+++ Web Browser/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BingBrowser.Navigate("https://www.bing.com/");
+        }
+
+        // Reset button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BingBrowser.Refresh();
+        }
+
+        // Close button
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // For  bing browser
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            BingBrowser.Navigate("https://www.bing.com/search?q=" + SearchTextBox.Text);
+        }
+
+        // Go forward
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            if (!BingBrowser.GoForward())
+                MessageBox.Show("There is no page to go forward to!", "Can't go forward!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }   
+
+        // Go backward
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            // Check if we are at the default page
+            if (BingBrowser.Url.AbsolutePath == "https://www.bing.com/")
+                return;
+
+            if (!BingBrowser.GoBack())
+                BingBrowser.Navigate("https://www.bing.com/");
+
+        }
+
+        // Home button
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            if (BingBrowser.Url.AbsolutePath != "https://www.bing.com/")
+                BingBrowser.Navigate("https://www.bing.com/");
+        }
+    }
+}
